dbd0e2c142fb388ffc6f905b04115ad697b4915c
SP3ALL <brockpenner1@gmail.com>
2015-02-19 18:53:05 +0900

    GD SP3 02
    bug on location of gateways

---
+++ Assets/Game Scripts/DistanceDetector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceDetector : MonoBehaviour {
+	public Transform targetTransform; //the gnome's transform
+	public Transform theCamera; //the camera's transform
+	Vector2 source; //the gateway
+	Vector3 target; //the gnome
+
+	// Use this for initialization
+	void Start () {
+		//assign the x and z position to the source var
+		source = new Vector2 (transform.position.x, transform.position.y);
+		print (source);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//update the target's location
+		target = new Vector2 (targetTransform.position.x, targetTransform.position.z);
+		print (target);
+		//get the distance between the target and source
+		float dist = Vector3.Distance (source, target);
+		if (dist < 3.0f && dist > .5f) {
+			Vector3 pos = theCamera.transform.localPosition; //make a variable to hold the current local position of the camera
+			pos.z = 3.0f - dist; // assign the inverse of the distance to the z part of the temporary variable
+			theCamera.transform.localPosition = pos; //assign the new position
+		} //else
+			//print (""); //clear the status line
+	}
+}
--- Assets/Game Scripts/SensorDoors.cs
+++ Assets/Game Scripts/SensorDoors.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class SensorDoors : MonoBehaviour {
 	public AnimationClip clipOpen; //the open animation
 	public AnimationClip clipClose; //the close animation
+//	public SmoothFollow follow; //the camera's SmoothFollow script
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,8 @@ public class SensorDoors : MonoBehaviour {
 	//open the gates
 	void OnTriggerEnter(Collider defender) {
 		if (defender.gameObject.tag == "Player") {
+//			follow.distance = 1.15f; //change the SmoothFollow distance
+//			follow.height = 0.5f; //change the SmoothFollow distance
 			animation.Play(clipOpen.name);
 			audio.Play ();//play the clip in the audio component
 		}
@@ -23,6 +26,8 @@ public class SensorDoors : MonoBehaviour {
 	//close the gates
 	void OnTriggerExit(Collider defender) {
 		if (defender.gameObject.tag == "Player") {
+//			follow.distance = 2.8f; //revert the SmoothFollow distance
+//			follow.height = 1.8f; //revert the SmoothFollow distance
 			animation.Play(clipClose.name);
 			audio.Play ();//play the clip in the audio component
 		}
